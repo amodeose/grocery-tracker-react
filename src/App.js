83cfc0ffc8bc4007e6a372b7b58e6951585ef4c3
amodeose <amodeosa@gmail.com>
2022-08-30 20:14:30 -0400

    list state and add to list logic

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { Fragment } from "react";
 import Header from "./components/Header";
 import NewEntry from "./components/NewEntry";
 import List from "./components/List";
+import { useState } from "react";
 
 function App() {
+
+  const [list, setList] = useState([])
+
+  const addToList = item => {
+    setList(prev => [item, ...prev]);
+  }
+
   return (
     <Fragment>
       <Header />
-      <NewEntry />
-      <List />
+      <NewEntry addToList={addToList}/>
+      <List list={list}/>
     </Fragment>
   );
 }
