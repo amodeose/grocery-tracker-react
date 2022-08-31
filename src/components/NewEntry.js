f5c005b2f0f7cc5d13df70ee8bff6a05a6e15fd7
amodeose <amodeosa@gmail.com>
2022-08-30 20:11:05 -0400

    input/state two way binding

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -1,10 +1,23 @@
 import classes from "./NewEntry.module.css";
+import { useState } from "react";
 
 const NewEntry = (props) => {
+
+    const [input, setInput] = useState("");
+    
+    const handleChange = event => {
+        const { value } = event.target;
+        setInput(value);
+    }
+
+    const handleAdd = () => {
+        setInput('');
+    }
   return (
     <div className={classes['new-entry']}>
-      <input />
-      <button>Add</button>
+      <input onChange={handleChange} value={input}/>
+      <button onClick={handleAdd}>Add</button>
+      {input}
     </div>
   );
 };
