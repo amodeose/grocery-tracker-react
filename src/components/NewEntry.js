19f4461259985725f45f1338058577f471de79bd
amodeose <amodeosa@gmail.com>
2022-08-30 20:05:08 -0400

    NewEntry styling

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -1,7 +1,12 @@
 import classes from "./NewEntry.module.css";
 
-const NewEntry = props => {
-    
+const NewEntry = (props) => {
+  return (
+    <div className={classes['new-entry']}>
+      <input />
+      <button>Add</button>
+    </div>
+  );
 };
 
 export default NewEntry;
