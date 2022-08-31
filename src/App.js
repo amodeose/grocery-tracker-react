9b92ba456d6b61616ca72082859d8924bd278ff8
amodeose <amodeosa@gmail.com>
2022-08-30 21:06:47 -0400

    store list state in local storage

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import { Fragment } from "react";
 import Header from "./components/Header";
 import NewEntry from "./components/NewEntry";
 import List from "./components/List";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
-  const [list, setList] = useState([])
+  const getLocalStorage = () => {
+    const list =localStorage.getItem('list');
+    if (list) {
+      return JSON.parse(list);
+    } else {
+      return [];
+    }
+  };
+
+  const [list, setList] = useState(getLocalStorage());
 
   const addToList = item => {
     setList(prev => [item, ...prev]);
-  }
+  };
 
   const deleteItem = id => {
     setList(prev => prev.filter((item, index) => index != id))
-  }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
 
   return (
     <Fragment>
@@ -23,6 +36,6 @@ function App() {
       {list.length > 0 && <List list={list} delete={deleteItem}/>}
     </Fragment>
   );
-}
+};
 
 export default App;
