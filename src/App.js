4c4f64194271f5907894e7936a0f5d60960c00dd
amodeose <amodeosa@gmail.com>
2022-09-01 21:31:33 -0400

    fade in and out whole List component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Header from "./components/Header";
 import NewEntry from "./components/NewEntry";
 import List from "./components/List";
 import { useState, useEffect } from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 function App() {
-
   const getLocalStorage = () => {
-    const list =localStorage.getItem('list');
+    const list = localStorage.getItem("list");
     if (list) {
       return JSON.parse(list);
     } else {
@@ -17,25 +17,33 @@ function App() {
 
   const [list, setList] = useState(getLocalStorage());
 
-  const addToList = item => {
-    setList(prev => [item, ...prev]);
+  const addToList = (item) => {
+    setList((prev) => [...prev, item]);
   };
 
-  const deleteItem = id => {
-    setList(prev => prev.filter((item, index) => index != id))
+  const deleteItem = (id) => {
+    setList((prev) => prev.filter((item, index) => index != id));
   };
 
   useEffect(() => {
-    localStorage.setItem('list', JSON.stringify(list));
+    localStorage.setItem("list", JSON.stringify(list));
   }, [list]);
 
   return (
     <Fragment>
       <Header />
-      <NewEntry addToList={addToList}/>
-      {list && <List list={list} delete={deleteItem}/>}
+      <NewEntry addToList={addToList} />
+      <CSSTransition
+        mountOnEnter
+        unmountOnExit
+        in={list.length > 0}
+        classNames="fade"
+        timeout={500}
+      >
+        <List list={list} delete={deleteItem} />
+      </CSSTransition>
     </Fragment>
   );
-};
+}
 
 export default App;
