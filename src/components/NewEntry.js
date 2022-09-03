a927700f5a3f0433d1bade6d96435c4cdfd63684
amodeose <amodeosa@gmail.com>
2022-09-02 21:29:52 -0400

    disable button if input empty

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -4,21 +4,21 @@ import { useState } from "react";
 const NewEntry = (props) => {
   const [input, setInput] = useState("");
 
+  const inputIsValid = input.trim() !== "";
+
   const handleChange = (event) => {
     const { value } = event.target;
     setInput(value);
   };
 
   const handleAdd = () => {
-    if (input.length > 0) {
       props.addToList(input);
       setInput("");
-    }
   };
   return (
     <div className={classes["new-entry"]}>
-      <input onChange={handleChange} value={input} />
-      <button onClick={handleAdd}>Add</button>
+      <input onChange={handleChange} placeholder="Enter a new item" value={input} />
+      <button onClick={handleAdd} disabled={!inputIsValid}>Add</button>
     </div>
   );
 };
